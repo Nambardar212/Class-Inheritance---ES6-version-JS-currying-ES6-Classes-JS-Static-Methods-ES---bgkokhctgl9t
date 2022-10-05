b543a8b8e3c1b66aecde7acb85d3b34587dd01d7
NIKHIL SAINI <40213598+Nambardar212@users.noreply.github.com>
2022-10-05 18:17:30 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,15 @@
 
 
 // export class using module.exports
+var Shape = require('./shape.js');
+
+class Circle extends Shape {
+     constructor(color) {
+          super();
+     }
+     calculateArea() {
+
+     }
+}
+
+module.exports = Circle;
