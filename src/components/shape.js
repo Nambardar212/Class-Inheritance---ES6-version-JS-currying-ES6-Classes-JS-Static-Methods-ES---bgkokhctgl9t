31b74081b050d7439ef5cd50706a14f9a1cb74d2
NIKHIL SAINI <40213598+Nambardar212@users.noreply.github.com>
2022-10-05 18:18:15 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
 
 // export class using module.exports
+class Shape {
+    constructor() {
+         this.color = "green";
+    }
+    drawShape() {
+
+    }
+    calculateArea() {
+
+    }
+}
+
+module.exports = Shape;
