0d3800ed9ed941bbe67cba7fbfa8646f726e1e51
NIKHIL SAINI <40213598+Nambardar212@users.noreply.github.com>
2022-10-15 12:27:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import Shape from "./components/shape.js";
+import Circle from "./components/circle.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
